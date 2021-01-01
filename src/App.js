7a665b7fdc1cd9661e3ae4c03d813ca0146c8c8e
Erwan Béguin <erwan.beguin@gmail.com>
2021-01-01 16:39:52 +0100

    nodes

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Grommet, Box,  } from "grommet";
+
+
+
+const theme = {
+  global: {
+    colors: {
+      brand: '#cc0000',
+      back: "#292929",
+      card: "#bfdbf7",
+      accent: "#994650",
+      ok: '#00C781',
+    },
+    font: {
+      family: "Roboto",
+      size: "18px",
+      height: "20px",
+    },
+  },
+};
+
+function App() {
+  return (
+    <Grommet theme={theme} full>
+      <Box fill align="center" justify="center" background="back">
+        
+      </Box>
+  
+    </Grommet>
+  );
+}
+
+export default App;
